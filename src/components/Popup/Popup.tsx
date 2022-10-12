55adda64109cbe4464bee38e301af3732bc05155
tnty <tnty.working@gmail.com>
2022-10-12 18:00:18 +0300

    feat: open/close popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,14 +1,22 @@
 import React, { FC } from 'react';
 import styles from './Popup.module.scss';
+import {useParams} from "react-router-dom";
 
 const Popup: FC = () => {
-  return (
-      <div className={styles.popup}>
-          <div className={styles.popup__content}>
-              <h1>POPUP!</h1>
-          </div>
-      </div>
-  );
+    const params = useParams();
+
+    if (params?.id) {
+        return (
+            <div className={styles.popup}>
+                <div className={styles.popup__content}>
+                    <h1>POPUP!</h1>
+                </div>
+            </div>
+        );
+    }
+
+
+    return null;
 };
 
 export default Popup;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import {useRoutes} from "react-router-dom";
 import MainPage from "@/pages/MainPage/MainPage";
+import Popup from "@/components/Popup/Popup";
 
 const AppRoutes = () => useRoutes([
-    {path: "/", element: <MainPage/>}
+    {
+        path: "/",
+        element: <MainPage/>,
+        children: [
+            {
+                path: "/task/:id",
+                element: <Popup />
+            }
+        ]
+    }
 ]);
 
 export default AppRoutes;
