c923b32a82eab35f56e386dd17a725ec58d03a4f
tnty <tnty.working@gmail.com>
2022-10-12 18:07:43 +0300

    refactor: open/close popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,11 +1,16 @@
-import React, { FC } from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import styles from './Popup.module.scss';
 import {useParams} from "react-router-dom";
 
 const Popup: FC = () => {
     const params = useParams();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    if (params?.id) {
+    useEffect(() => {
+        setIsOpen(Boolean(params.id));
+    }, [params])
+
+    if (isOpen) {
         return (
             <div className={styles.popup}>
                 <div className={styles.popup__content}>
