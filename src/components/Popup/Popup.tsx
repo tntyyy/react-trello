fdf0069f63a1b4769c0a2b4d9fde52044fcf9db3
tnty <tnty.working@gmail.com>
2022-10-13 09:04:04 +0300

    fix: bug with popup url

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,18 +1,23 @@
 import React, {FC, useEffect, useState} from 'react';
 import styles from './Popup.module.scss';
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 
 const Popup: FC = () => {
     const params = useParams();
+    const navigate = useNavigate();
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
     useEffect(() => {
         setIsOpen(Boolean(params.id));
-    }, [params])
+    }, [params]);
+
+    const handleClosePopup = () => {
+        navigate("/");
+    }
 
     if (isOpen) {
         return (
-            <div className={styles.popup} onClick={() => setIsOpen(false)}>
+            <div className={styles.popup} onClick={() => handleClosePopup()}>
                 <div className={styles.popup__content} onClick={(e) => e.stopPropagation()}>
                     <input className={styles.popup__input} type="text" defaultValue={"name of task"}/>
                     <textarea className={styles.popup__input} placeholder={"Enter description..."} defaultValue={"description of task"}></textarea>
