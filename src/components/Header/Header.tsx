ee87c08b90fb6b36c9efe0f35e14586095f263d5
tnty <tnty.working@gmail.com>
2022-10-13 17:15:41 +0300

    feat: move create board logic to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,30 @@
-import React, { FC } from 'react';
+import React, {FC, useContext} from 'react';
 import styles from './Header.module.scss';
+import {IBoard} from "@/types/board";
+import {v4 as uuidv4} from "uuid";
+import storageManager from "@/utils/storageManager";
+import {BoardsContext} from "@/context/boardsContext";
 
-interface IHeaderProps {
-    createBoard: () => void;
-}
 
-const Header: FC<IHeaderProps> = ({createBoard}) => {
+const Header: FC = () => {
+    const storage = storageManager.getInstance();
+    const {setBoards} = useContext(BoardsContext);
+
+    const createNewBoard = () => {
+        const board: IBoard = {
+            id: uuidv4(),
+            name: "default board",
+            tasks: []
+        };
+
+        storage.setBoard(board);
+        setBoards((prevState) => [...prevState, board]);
+    }
 
   return (
       <header className={styles.header}>
           <h1 className={styles.logo}>Trello</h1>
-          <button className={styles.createList} onClick={() => createBoard()}>Create board</button>
+          <button className={styles.createList} onClick={() => createNewBoard()}>Create board</button>
       </header>
   );
 };
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,31 +3,15 @@ import styles from './MainPage.module.scss';
 import Header from "@/components/Header/Header";
 import Board from "@/components/Board/Board";
 import Popup from "@/components/Popup/Popup";
-import storageManager from "@/utils/storageManager";
-import {IBoard} from "@/types/board";
-import {v4 as uuidv4} from "uuid";
 import {BoardsContext} from "@/context/boardsContext";
 
 const MainPage: FC = () => {
-    const storage = storageManager.getInstance();
-    const {boards, setBoards} = useContext(BoardsContext);
-
-    const createNewBoard = async () => {
-        const board: IBoard = {
-            id: uuidv4(),
-            name: "default board",
-            tasks: []
-        };
-
-        storage.setBoard(board);
-        setBoards((prevState) => [...prevState, board]);
-        console.log(boards);
-    }
+    const {boards} = useContext(BoardsContext);
 
     return (
       <>
         <Popup />
-        <Header createBoard={createNewBoard}/>
+        <Header/>
         <div className={styles.content}>
             {
                 boards && boards.map((board) =>
